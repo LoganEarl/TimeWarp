1d88f016a7e524a35597b1c2ab93421cd2eb03f6
PavelTr91 <53226987+PavelTr91@users.noreply.github.com>
2020-01-26 21:12:06 -0800

    Basics

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+    public Vector3 offset;
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start() {
+        offset = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float horizontal = Input.GetAxis("Mouse X") * rotationSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        float desiredYAngle = target.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredYAngle, 0);
+        transform.position = target.position - rotation * offset;
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravityScale;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start() {
+        controller = GetComponent<CharacterController>();
+        moveDirection = new Vector3();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        moveDirection = new Vector3() {
+            x = moveSpeed * Input.GetAxis("Horizontal"),
+            y = moveDirection.y,
+            z = moveSpeed * Input.GetAxis("Vertical")
+        };
+
+        if (controller.isGrounded) {
+            moveDirection.y = 0;
+            if (Input.GetButtonDown("Jump")) {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y += Physics.gravity.y * gravityScale * Time.fixedDeltaTime;
+        controller.Move(moveDirection * Time.fixedDeltaTime);
+    }
+}
