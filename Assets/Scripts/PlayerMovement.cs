05b8b18dd11199f2e4e476b2b1ccb9c1d0d2c04d
Evo <evolve95@gmail.com>
2020-01-29 17:28:21 -0800

    Moving & Animated Player Model
    - Found a free basic humanoid model with animations
    - Made Player GameObject
    - Animated Player
    - Phsyics on Player
    - Player Movement works

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+    public float moveSpeed = 20f;
+    Vector3 movement;
+    Quaternion rotation = Quaternion.identity;
+
+    Animator animator;
+    Rigidbody rgbd;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rgbd = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        movement.Set(horizontal, 0f, vertical);
+        movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
+        rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        rgbd.MovePosition(rgbd.position + movement * moveSpeed);
+        rgbd.MoveRotation(rotation);
+    }
+}
