85c0d8d227296c9c2b46ec63bd32a6c5b03a8701
Evo <evolve95@gmail.com>
2020-02-06 13:51:52 -0800

    Only bottom half of torso moves for whatever reason
    yaaaay

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,8 +10,6 @@ public class Bullet : MonoBehaviour
     public float bouncesLeft = 400;
     public float maxLifeTime = 10f;
 
-    private float prevMag = 650f;
-
     void Start() {
         Destroy(gameObject, maxLifeTime);
     }
@@ -20,20 +18,11 @@ public class Bullet : MonoBehaviour
         Rigidbody bulletInstance = GetComponent<Rigidbody>();
         Collider other = collision.collider;
 
-        if (other.tag == "Player" + playerNumber)
-            bulletInstance.isKinematic = false;
-
         if (other.tag == "Wall" && bouncesLeft != 0) bouncesLeft--;
         else if (other.tag == "Wall") Destroy(gameObject, 0f);
-        else if (other.tag != "Player" + playerNumber) {
+        else if (other.tag != "Player") {
             other.GetComponent<PlayerHealth>().TakeDamage(bulletDmg);
             Destroy(gameObject, 0f);
         }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.collider.tag == "Player" + playerNumber)
-            GetComponent<Rigidbody>().isKinematic = false;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,44 +5,47 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public int playerNumber = 1;
-    public float turnSpeed = 20f;
-    public float moveSpeed = 20f;
-    Vector3 movement;
-    Quaternion rotation = Quaternion.identity;
+    public float turnSpeed = 20f, moveSpeed = 20f;
+    Vector3 movement, mousePosition;
+    Quaternion rotation = Quaternion.identity, torsoRotation = Quaternion.identity;
 
     Animator animator;
-    Rigidbody rgbd;
+    Rigidbody rgbd, torsoRGBD;
 
     void Start() {
         animator = GetComponent<Animator>();
         rgbd = GetComponent<Rigidbody>();
+        torsoRGBD = GetComponentInChildren<Rigidbody>();
     }
 
     void FixedUpdate() {
-        float horizontal = Input.GetAxis("Horizontal" + playerNumber);
-        float vertical = Input.GetAxis("Vertical" + playerNumber);
+        float horizontalMove = Input.GetAxis("Horizontal" + playerNumber);
+        float verticalMove = Input.GetAxis("Vertical" + playerNumber);
 
-        movement.Set(horizontal, 0f, vertical);
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 2f;
+        mousePosition.y = 0;
+
+        movement.Set(horizontalMove, 0f, verticalMove);
         movement.Normalize();
 
-        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool hasHorizontalInput = !Mathf.Approximately(horizontalMove, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(verticalMove, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
 
-        animator.SetBool("IsWalking", isWalking);
+        //animator.SetBool("IsWalking", isWalking);
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
+        Vector3 desiredLook = Vector3.RotateTowards(torsoRGBD.transform.forward, mousePosition, turnSpeed * Time.deltaTime, 0f);
         rotation = Quaternion.LookRotation(desiredForward);
-    }
+        //torsoRotation = Quaternion.LookRotation(desiredForward);
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.tag == "Wall") {
-            rgbd.MovePosition(new Vector3());
-        }
+        rgbd.MovePosition(rgbd.position + movement * moveSpeed);
+        //torsoRGBD.MovePosition(torsoRGBD.position + movement * moveSpeed);
+        rgbd.MoveRotation(rotation);
+        //torsoRGBD.MoveRotation(rotation);
     }
 
     void OnAnimatorMove() {
-        rgbd.MovePosition(rgbd.position + movement * moveSpeed);
-        rgbd.MoveRotation(rotation);
     }
 }
