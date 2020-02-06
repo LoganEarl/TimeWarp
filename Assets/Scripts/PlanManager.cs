a5adac84aae9c52030d7a6db7a55db61bb9e49ed
LoganEarl <iralogan42@gmail.com>
2020-02-05 16:15:00 -0800

    Fixed the Janky recording code

--- Assets/Scripts/PlanManager.cs
+++ Assets/Scripts/PlanManager.cs
@@ -12,29 +12,8 @@ public class PlanManager : MonoBehaviour, IGameMode
     private int roundNumber = -1; //starts at -1, but first match is 0. This is so NextMatch() doesnt need edge case checks
 
     private Dictionary<string, GameObject> loadedPlayerModels = new Dictionary<string, GameObject>();
-    private List<GameObject> currentPlayers = new List<GameObject>();
-
-    private List<PlayerController>[] playerControllers;
-    //TODO: This is temporary. I want to redo this with actual replay objects later
-    private List<List<PlayerSnapshot>>[] playerRecordings; //player,match index,current step state
-
-    /*
-    Player num Array[
-        Round Number List[
-            R1 Frames[
-                Frame Snapshot,
-                Frame Snapshot
-            ]
-            R2 Frames[
-                Frame Snapshot,
-                Frame Snapshot
-            ]
-        ]
-    ]
-
-
-     */
-
+    private PlanPlayerManager[] playerManagers = null;
+    private List<GameObject> playerObjects = new List<GameObject>();
 
     private static readonly int MAX_STEPS = 360;
 
@@ -51,19 +30,8 @@ public class PlanManager : MonoBehaviour, IGameMode
             if (stepNumber > MAX_STEPS)
                 NextMatch();
             stepNumber++;
-            for (int curPlayer = 0; curPlayer < numPlayers; curPlayer++)
-            {
-                for(int roundCounter = 0; roundCounter <= roundNumber; roundCounter++)
-                {
-                    List<PlayerSnapshot> currentReplay = playerRecordings[curPlayer][roundCounter];
-                    PlayerController currentController = playerControllers[curPlayer][roundCounter];
-
-                    if (roundCounter == roundNumber)
-                        currentReplay.Add(currentController.GetSnapshot());
-                    else
-                        currentController.SetSnapshot(currentReplay[stepNumber]);
-                }
-            }
+            foreach (PlanPlayerManager player in playerManagers)
+                player.Step(stepNumber);
         }
     }
 
@@ -71,75 +39,131 @@ public class PlanManager : MonoBehaviour, IGameMode
     {
         stepNumber = -1;
         roundNumber++;
-        UnloadPlayers();
-        LoadPlayers();   //reloads the scene with the new data
+        foreach (PlanPlayerManager player in playerManagers)
+            player.FinishSequence();
+        LoadNewPlayers();
     }
 
     public void Setup(int numPlayers, ILevelConfig levelConfig)
     {
         this.levelConfig = levelConfig;
         this.numPlayers = numPlayers;
-        playerControllers = new List<PlayerController>[numPlayers];
-        playerRecordings = new List<List<PlayerSnapshot>>[numPlayers];
+        playerManagers = new PlanPlayerManager[numPlayers];
+        for (int i = 0; i < numPlayers; i++)
+            playerManagers[i] = new PlanPlayerManager();
     }
 
     public void Begin()
     {
         if (levelConfig != null)
             SceneManager.LoadScene(levelConfig.GetSceneName(), LoadSceneMode.Single);
+
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if(levelConfig != null && scene.name.Equals(levelConfig.GetSceneName()))
+        if (levelConfig != null && scene.name.Equals(levelConfig.GetSceneName()))
         {
             begun = true;
             SceneManager.SetActiveScene(SceneManager.GetSceneByName(levelConfig.GetSceneName()));
             NextMatch();
-        }   
-    }
-
-    private void UnloadPlayers()
-    {
-        foreach(GameObject obj in currentPlayers)
-        {
-            Destroy(obj);
         }
-        currentPlayers.Clear();
     }
 
-    private void LoadPlayers()
+    private void LoadNewPlayers()
     {
-        //instantiate matchNumber * numPlayers player prefabs in the current scene
-        for(int curPlayer = 0; curPlayer < numPlayers; curPlayer++)
+        for (int curPlayer = 0; curPlayer < numPlayers; curPlayer++)
         {
-            playerControllers[curPlayer] = new List<PlayerController>();
-            if(playerRecordings[curPlayer] == null)
-                playerRecordings[curPlayer] = new List<List<PlayerSnapshot>>();
+            PlanPlayerManager manager = playerManagers[curPlayer];
 
-            for(int roundCounter = 0; roundCounter <= roundNumber; roundCounter++)
+            string assetName = levelConfig.GetPlayerModelName(curPlayer, roundNumber);
+            GameObject playerPrefab;
+            if (loadedPlayerModels.ContainsKey(assetName))
+                playerPrefab = loadedPlayerModels[assetName];
+            else
             {
-                string assetName = levelConfig.GetPlayerModelName(curPlayer, roundCounter);
-                GameObject playerPrefab;
-                if (loadedPlayerModels.ContainsKey(assetName))
-                    playerPrefab = loadedPlayerModels[assetName];
-                else
-                {
-                    playerPrefab = Resources.Load(assetName) as GameObject;
-                    loadedPlayerModels[assetName] = playerPrefab;
-                }
-
-                GameObject player = Instantiate(playerPrefab, levelConfig.GetPlayerSpawnPosition(curPlayer, roundCounter), Quaternion.identity);
-                currentPlayers.Add(player);
-                PlayerController playerController = player.GetComponent<PlayerController>();
-                playerController.SetPlayerInformation(curPlayer, roundCounter);
-
-                playerController.SetUseSnapshots(roundCounter != roundNumber);
-                playerControllers[curPlayer].Add(playerController);
-
-                if(roundCounter == roundNumber)
-                    playerRecordings[curPlayer].Add(new List<PlayerSnapshot>());
+                playerPrefab = Resources.Load(assetName) as GameObject;
+                loadedPlayerModels[assetName] = playerPrefab;
             }
+
+            GameObject player = Instantiate(playerPrefab, levelConfig.GetPlayerSpawnPosition(curPlayer, roundNumber), Quaternion.identity);
+            playerObjects.Add(player);
+            PlayerController playerController = player.GetComponent<PlayerController>();
+            playerController.SetPlayerInformation(curPlayer, roundNumber);
+            if (!manager.RecordExistsForMatch(roundNumber))
+                manager.AppendNewRecording(playerController);
+        }
+    }
+
+    private class PlanPlayerManager
+    {
+        private List<MatchRecordingManager> playerRecordings = new List<MatchRecordingManager>();
+
+        internal void AppendNewRecording(PlayerController controller)
+        {
+            playerRecordings.Add(new MatchRecordingManager(controller));
+        }
+
+        internal void Step(int stepNumber)
+        {
+            foreach (MatchRecordingManager recording in playerRecordings)
+            {
+                recording.AppendNextSnapshot();
+                recording.UtilizeFrame(stepNumber);
+            }
+        }
+
+        internal void FinishSequence()
+        {
+            foreach (MatchRecordingManager recording in playerRecordings)
+            {
+                recording.Finish();
+            }
+        }
+
+        internal bool RecordExistsForMatch(int matchNum)
+        {
+            return playerRecordings.Count > matchNum && matchNum >= 0;
+        }
+    }
+
+    private class MatchRecordingManager
+    {
+        private bool recordingComplete = false;
+        private PlayerController playerController;
+        private List<PlayerSnapshot> snapshots = new List<PlayerSnapshot>();
+
+        internal MatchRecordingManager(PlayerController playerController)
+        {
+            this.playerController = playerController;
+            playerController.SetUseSnapshots(false);
+        }
+
+        internal void AppendNextSnapshot()
+        {
+            if (!recordingComplete)
+                snapshots.Add(playerController.GetSnapshot());
+        }
+
+        internal PlayerSnapshot UtilizeFrame(int frameNum)
+        {
+            if (recordingComplete && snapshots.Count > 0)
+            {
+                if (frameNum >= 0 && frameNum < snapshots.Count)
+                    playerController.SetSnapshot(snapshots[frameNum]);
+                else if (frameNum < 0)
+                    playerController.SetSnapshot(snapshots[0]);
+                else
+                    playerController.SetSnapshot(snapshots[snapshots.Count - 1]);
+            }
+            return null;
+        }
+
+        internal void Finish()
+        {
+            recordingComplete = true;
+            playerController.SetUseSnapshots(true);
         }
     }
 }
+
