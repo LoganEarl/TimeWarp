2a17faa368f16ca50affc24166be56d47b8c79f4
Evo <evolve95@gmail.com>
2020-02-08 14:24:14 -0800

    Fixed Animation Issues
    - The animations themselves just needed to be set to allow looping, and with that I think all the animations work pretty well!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,34 +6,22 @@ public class PlayerMovement : MonoBehaviour
 {
     public int playerNumber = 1;
     public float turnSpeed = 20f, moveSpeed = 0.05f;
-    public Transform targetingCursor, fireTransform;
-    Vector3 movement, mousePosition, aimAt, lastPosition, curVelocity;
-    Quaternion desiredRotation = Quaternion.identity, torsoRotation = Quaternion.identity;
-
-    private float weaponHeight;
-    Animator animator;
-    Rigidbody rgbd;
+    public Transform targetingCursor;
+    private Vector3 movement, mousePosition;
+    private Quaternion desiredRotation = Quaternion.identity;
+    
+    private Animator animator;
+    private Rigidbody rgbd;
 
     void Start() {
         animator = GetComponentInChildren<Animator>();
         rgbd = GetComponent<Rigidbody>();
-        weaponHeight = fireTransform.TransformPoint(fireTransform.position).y;
     }
 
     void FixedUpdate() {
         float horizontalMove = Input.GetAxis("Horizontal" + playerNumber);
         float verticalMove = Input.GetAxis("Vertical" + playerNumber);
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(Camera.main.transform.position, ray.direction, out hit, Mathf.Infinity))
-            mousePosition = new Vector3(hit.point.x, 0.00001f, hit.point.z);
-
-        targetingCursor.position = mousePosition;
-        curVelocity = (rgbd.position - lastPosition).normalized;
-        Vector3 aimAt = new Vector3(mousePosition.x, mousePosition.y, mousePosition.z);
-        
         movement.Set(horizontalMove, 0f, verticalMove);
         movement.Normalize();
 
@@ -42,27 +30,35 @@ public class PlayerMovement : MonoBehaviour
         bool isIdle = !(hasHorizontalInput || hasVerticalInput);
         animator.SetBool("IsIdle", isIdle);
 
-        float walkingAngle = Vector3.SignedAngle(transform.forward, curVelocity, Vector3.up);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(Camera.main.transform.position, ray.direction, out RaycastHit hit, Mathf.Infinity))
+            mousePosition = new Vector3(hit.point.x, 0.00001f, hit.point.z);
+
+        targetingCursor.position = mousePosition;
         int animDirection = 2;
 
-        if (walkingAngle > -45f && walkingAngle < 45f) animDirection = 0;
-        else if (walkingAngle > 45f && walkingAngle < 135f) animDirection = 1;
-        else if (walkingAngle > -135f && walkingAngle < -45f) animDirection = 3;
+        if (!isIdle) {
+            float walkingAngle = Vector3.SignedAngle(transform.forward, movement, Vector3.up);
 
-        animator.SetInteger("WalkingAngle", animDirection);
+            if (walkingAngle > -45f && walkingAngle < 45f) animDirection = 0;
+            else if (walkingAngle > 45f && walkingAngle < 135f) animDirection = 1;
+            else if (walkingAngle > -135f && walkingAngle < -45f) animDirection = 3;
 
-        aimAt = aimAt - transform.position;
-        Debug.Log("transform Position: " + transform.forward + ", Aiming at: " + aimAt);
+            animator.SetInteger("WalkingAngle", animDirection);
+        }
+
+        Debug.Log(isIdle + ", animDirection: " + animDirection);
+
+        Vector3 aimAt = mousePosition - transform.position;
 
         Vector3 desiredLook = Vector3.RotateTowards(transform.forward, aimAt, turnSpeed * Time.deltaTime, 0f);
         desiredLook.y = 0f;
 
-        desiredRotation = Quaternion.LookRotation(desiredLook);
-        lastPosition = rgbd.position;
+        Quaternion desiredRotation = Quaternion.LookRotation(desiredLook);
 
         rgbd.MovePosition(rgbd.position + movement * moveSpeed);
         rgbd.MoveRotation(desiredRotation);
-
     }
 
     void OnAnimatorMove() {
