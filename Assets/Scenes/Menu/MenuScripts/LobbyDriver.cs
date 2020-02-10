24677acb2c6ac67c49cf1e43f3b2febc75affe10
muangkongyang <50885743+muangkongyang@users.noreply.github.com>
2020-02-09 16:10:31 -0800

    Lobby Menu Button Interact
    Added in:
    - level image placeholders
    - Lobby Driver methods

--- Assets/Scenes/Menu/MenuScripts/LobbyDriver.cs
+++ Assets/Scenes/Menu/MenuScripts/LobbyDriver.cs
@@ -17,7 +17,7 @@ public class LobbyDriver : MonoBehaviour {
 
     public PlanManager planGameMode;
 
-    // Added this in to make start button uninteractable at the beginning
+    // Added Start() in to make start button uninteractable at the beginning
     public void Start()
     {
         startButton.interactable = false;
@@ -34,6 +34,28 @@ public class LobbyDriver : MonoBehaviour {
         selectedGameMode = planGameMode;
         CheckLoadButtonAvailability();
     }
+
+    // Added for levels and game modes not created yet
+    public void SelectTestLevelWithIndex(int level) // maybe pass in levels by an index? or by name
+    {
+        if (level == 1)
+            selectedLevel = new TestLevelConfig();
+        else if(level == 2 || level == 3)
+            selectedLevel = null;
+
+        CheckLoadButtonAvailability();
+    }
+
+    public void SelectGameMode(string gameModeName)
+    {
+        if (gameModeName.Equals("Plan"))
+            selectedGameMode = planGameMode;
+        else
+            selectedGameMode = null;
+
+        CheckLoadButtonAvailability();
+    }
+    // End added stuff
 
     public void GoToMainMenu()
     {
