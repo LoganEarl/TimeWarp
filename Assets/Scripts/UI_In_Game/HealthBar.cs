c01cc67503d333c61465c45e55cb4ec35a9b13ab
muangkongyang <50885743+muangkongyang@users.noreply.github.com>
2020-02-16 17:12:59 -0800

    Merging with Logan
    Adding in Logan's UI current endpoint updates.

---
+++ Assets/Scripts/UI_In_Game/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * This class maintains the health bar of a player. Taking damage will cause the
+ * slider to change in value in accordance to how much health that player has left.
+ * Other classes may reference this to control the UI of the player health bar.
+ */
+
+public class HealthBar : MonoBehaviour {
+    [SerializeField]
+    private Slider slider;
+
+    public PlayerHealth playerHealth;
+    
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = 0;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = (int)slider.maxValue - health;
+    }
+}
