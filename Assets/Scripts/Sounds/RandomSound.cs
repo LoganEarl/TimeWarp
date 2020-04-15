76262e68dc55981551b85113ea02755322f8437c
Evo Sokoloff-Toney <evolve95@gmail.com>
2020-04-15 12:19:38 -0700

    Filled in the code for moving randomsound to master

--- Assets/Scripts/Sounds/RandomSound.cs
+++ Assets/Scripts/Sounds/RandomSound.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public abstract class RandomSound : MonoBehaviour
 {
-    
+    [SerializeField] private float[] activationChance;
+    [SerializeField] private AudioClip[] clips;
+
+    public AudioClip GetClip()
+    {
+        int randomSound = Mathf.RoundToInt(Random.value * (clips.Length - 1));
+        bool speaking = (Random.value * 100) <= activationChance[randomSound];
+
+        if (speaking)
+            return clips[randomSound];
+        else
+            return null;
+    }
 }
